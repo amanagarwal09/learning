df40cc0514cbaea034c38c52647fe86a180e7efd
amanagarwal09 <62548194+amanagarwal09@users.noreply.github.com>
2024-01-11 13:22:17 +0530

    Update TweetProducer.java

--- FSE1TweetApp/src/main/java/com/tweetapp/producer/TweetProducer.java
+++ FSE1TweetApp/src/main/java/com/tweetapp/producer/TweetProducer.java
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-
+//testing
 @Service
 @Slf4j
 public class TweetProducer {
