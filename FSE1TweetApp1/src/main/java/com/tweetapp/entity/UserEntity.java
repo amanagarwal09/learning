137079d48e3df8ceceab1ca36db28745a879243f
amanagarwal09 <62548194+amanagarwal09@users.noreply.github.com>
2024-01-11 13:22:59 +0530

    Update UserEntity.java

--- FSE1TweetApp1/src/main/java/com/tweetapp/entity/UserEntity.java
+++ FSE1TweetApp1/src/main/java/com/tweetapp/entity/UserEntity.java
@@ -6,7 +6,7 @@ import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
-
+//test
 @Getter
 @Setter
 @NoArgsConstructor
